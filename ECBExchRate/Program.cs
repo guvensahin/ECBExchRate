c1c59122f6c44eb8a5bbd98928896944498b369d
Güven Şahin <guven.sahin@outlook.com>
2022-03-17 16:10:59 +0300

    Add project files.

---
+++ ECBExchRate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ECBExchRate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Parse("2022-03-16");
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"); // last 90 days
+
+
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
+            nsmgr.AddNamespace("x", "http://www.ecb.int/vocabulary/2002-08-01/eurofxref");
+
+
+            string dateStr = dateTime.ToString("yyyy-MM-dd");
+            string xpath = string.Format("//x:Cube[@time='{0}']", dateStr);
+
+            XmlNode nodeDay = xmlDoc.SelectSingleNode(xpath, nsmgr);
+
+            XmlNodeList nodeList = nodeDay.ChildNodes;
+
+
+            // show on console
+            Console.WriteLine(dateStr);
+
+            foreach (XmlNode nodeCurrency in nodeList)
+            {
+                Console.WriteLine(String.Format("Currency: {0}, ExchRate: {1}",
+                    nodeCurrency.Attributes["currency"].Value,
+                    nodeCurrency.Attributes["rate"].Value));
+            }
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
